ce20e2d6fb696cd29445d4321f9d6e3399bc0803
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2022-02-15 18:47:38 -0800

    Server code for blob storage

--- app/src/storage/http.ts
+++ app/src/storage/http.ts
@@ -1,42 +1,45 @@
-const BASE = 'http://localhost:5000'
 
-export function getURI(id: string): string {
-  return `${BASE}/${id}`
-}
+export default class HTTP {
+  BASE: string = 'http://localhost:5000'
 
-export async function ids(): Promise<string[]> {
-  throw new Error('unimplemented')
-}
+  constructor(url?: string) {
+    if (url) this.BASE = url
+  }
 
-export async function getItem (id: string, actorId?: string): Promise<Uint8Array> { 
-  const response = await fetch(getURI(id))
-  if (response.status !== 200) throw new Error('No saved draft for doc with id=' + id)
-  const respbuffer = await response.arrayBuffer()
-  if (respbuffer.byteLength === 0) throw new Error('No saved draft for doc with id=' + id)
-  return new Uint8Array(respbuffer)
-}
+  async ids(): Promise<string[]> {
+    throw new Error('unimplemented')
+  }
 
-export async function setItem (id: string, binary: Uint8Array): Promise<void> {
-  let res = fetch(getURI(id), {
-    body: binary,
-    method: "post",
-    headers: {
-      "Content-Type": "application/octet-stream",
-    }
-  })
-  res.catch(err => {
-    console.error(err)
-  })
-  return
-}
+  async getItem (id: string, actorId?: string): Promise<Uint8Array> { 
+    const response = await fetch(this.getURI(id))
+    if (response.status !== 200) throw new Error('No saved draft for doc with id=' + id)
+    const respbuffer = await response.arrayBuffer()
+    if (respbuffer.byteLength === 0) throw new Error('No saved draft for doc with id=' + id)
+    return new Uint8Array(respbuffer)
+  }
 
-export async function deleteItem(id: string): Promise<Response> {
-  let binary = ''
-  return fetch(getURI(id), {
-    body: binary,
-    method: "post",
-    headers: {
-      "Content-Type": "application/octet-stream",
-    }
-  })
+  async setItem (id: string, binary: Uint8Array): Promise<void> {
+    fetch(this.getURI(id), {
+      body: binary,
+      method: "post",
+      headers: {
+        "Content-Type": "application/octet-stream",
+      }
+    })
+  }
+
+  async deleteItem(id: string): Promise<Response> {
+    let binary = ''
+    return fetch(this.getURI(id), {
+      body: binary,
+      method: "post",
+      headers: {
+        "Content-Type": "application/octet-stream",
+      }
+    })
+  }
+  getURI(id: string): string {
+    return `${this.BASE}/${id}`
+  }
+
 }
---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+let app = express()
+var options = {
+	inflate: true,
+	limit: '100kb',
+	type: 'application/octet-stream'
+};
+app.use(bodyParser.raw(options));
+app.use(cors());
+
+try { 
+	fs.mkdirSync(path.join(__dirname, 'data'))
+} catch (err) {
+	if (err.code !== 'EEXIST') {
+		console.error(err)
+	} 
+}
+
+app.get('/:id', (req, res) => {
+	let id = req.params.id
+	let filename = path.join(__dirname, 'data', id)
+	fs.stat(filename, (err, stats) => {
+		if (err) {
+			console.error(err)
+			res.status(404).send('Not found')
+		} else { 
+			res.sendFile(filename)
+			console.log('sending')
+		}
+	})
+})
+
+app.post('/:id', (req, res) => {
+	let id = req.params.id
+	fs.writeFileSync(path.join(__dirname, 'data', id), req.body)
+	res.status(200).send('ok')
+})
+
+module.exports = app
