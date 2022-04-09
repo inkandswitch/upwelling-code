5fccd0ea2a694d91ad0d61906eaa5b79422132db
Blaine Cook <romeda@gmail.com>
2022-04-09 09:10:50 -0700

    fix end-of-document position handling and add debug helper

--- app/src/prosemirror/utils/PositionMapper.ts
+++ app/src/prosemirror/utils/PositionMapper.ts
@@ -39,20 +39,23 @@ export const automergeToProsemirrorNumber = (
 
 export const prosemirrorToAutomergeNumber = (
   position: number,
+  draft: Draft,
   state: EditorState
 ): number => {
   let idx = 0
   let blocks = 0
   let offset = 0
+  let nudge = 0
   while (idx < state.doc.content.childCount) {
     let contentNode = state.doc.content.maybeChild(idx)
     if (!contentNode) continue
     let nodeSize = contentNode.nodeSize
     offset += nodeSize
 
-    // handle boundary case of empty nodes
-    // there is probably a more elegant way to do this.
-    if (offset > position) break
+    if (offset > position) {
+      if (nodeSize === 2 && offset === position) nudge = -1
+      break
+    }
     idx++
     blocks++
   }
@@ -62,7 +65,49 @@ export const prosemirrorToAutomergeNumber = (
 
   let diff = prosemirrorBlockCount - automergeBlockCount
 
-  return position - diff
+  let amPosition = position - diff + nudge
+
+  /*-- IN CASE OF POSITION MAPPING ISSUES
+   *
+   * reverseMap should equal proseMirrorPosition
+   *
+   * n.b. the last position of the document does not necessarily match, but
+   * that's okay because prosemirror likes to put the cursor _after_ the last
+   * closing tag, but insertions happen _inside_ that tag (or tags), but we
+   * don't have a way to reverse-map that and the behaviour is equivalent.
+   * I *think* the warnings that this code produces in that specific scenario
+   * are ignorable.
+   *
+   * UNBREAK COMMENT HERE
+   *--/
+
+  let am2pm = automergeToProsemirrorNumber(amPosition, draft)
+
+  console.log({
+    proseMirrorPosition: position,
+    automergeEquivalent: amPosition,
+    reverseMap: am2pm,
+    correct: am2pm === position,
+  })
+
+  if (am2pm !== position) {
+    console.log('Error: PM<->AM position mapping inconsistentcy detected.')
+    console.log({
+      prosemirrorState: state,
+      prosemirrorDoc: state.doc,
+      automergeDraft: draft,
+      automergeDraftText: `-->${draft.text}<--`,
+      math: {
+        idx,
+        blocks,
+        offset,
+        nudge,
+      },
+    })
+  }
+  /**/
+
+  return amPosition
 }
 
 export const prosemirrorToAutomerge = (
@@ -71,7 +116,7 @@ export const prosemirrorToAutomerge = (
   state: EditorState
 ): { start: number; end: number } => {
   return {
-    start: prosemirrorToAutomergeNumber(position.from, state),
-    end: prosemirrorToAutomergeNumber(position.to, state),
+    start: prosemirrorToAutomergeNumber(position.from, draft, state),
+    end: prosemirrorToAutomergeNumber(position.to, draft, state),
   }
 }
