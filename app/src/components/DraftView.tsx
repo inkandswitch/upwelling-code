a2ccf6f2ab912870c3cce415af43fd5105e564cb
Rae Mac <633012+okdistribute@users.noreply.github.com>
2022-04-10 21:24:39 +0200

    remove unused function

--- app/src/components/DraftView.tsx
+++ app/src/components/DraftView.tsx
@@ -401,12 +401,3 @@ export default function DraftView(props: DraftViewProps) {
     </div>
   )
 }
-
-function arrayEquals(a: Array<any>, b: Array<any>) {
-  return (
-    Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index])
-  )
-}
