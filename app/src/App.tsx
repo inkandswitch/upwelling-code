ffcd1a12546c1d7c765ea71d4fc3a4a10a11143d
Rae Mac <633012+okdistribute@users.noreply.github.com>
2022-04-14 13:18:23 -0700

    Add drag/drop support

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Redirect, Route } from 'wouter'
 import Documents from './Documents'
 import DraftView from './components/DraftView'
 import withDocument from './components/withDocument'
+import { useDropzone } from 'react-dropzone'
 import NoDocument from './components/NoDocument'
 import { useLocation } from 'wouter'
 import { Button } from './components/Button'
@@ -14,6 +15,25 @@ require('setimmediate')
 
 export default function App() {
   let [, setLocation] = useLocation()
+  const onDrop = useCallback((acceptedFiles) => {
+    const reader = new FileReader()
+
+    reader.onabort = () => console.log('file reading was aborted')
+    reader.onerror = () => console.log('file reading has failed')
+    reader.onload = async () => {
+      // Do whatever you want with the file contents
+      const binaryStr = reader.result
+      if (binaryStr) {
+        let upwell = await documents.toUpwell(Buffer.from(binaryStr))
+        await documents.storage.setItem(upwell.id, binaryStr)
+        setLocation('/' + upwell.id + '/stack')
+      }
+    }
+    reader.readAsArrayBuffer(acceptedFiles[0])
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
   async function newUpwell() {
     let id = nanoid()
     let doc = await documents.create(id, documents.author)
@@ -36,9 +56,17 @@ export default function App() {
       <Route path="/">
         {(params) => {
           return (
-            <NoDocument>
-              <Button onClick={newUpwell}>New Document</Button>
-            </NoDocument>
+            <div {...getRootProps()}>
+              <NoDocument>
+                <input {...getInputProps()} />
+                {isDragActive ? (
+                  <p>Drop the files here ...</p>
+                ) : (
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                )}
+                <Button onClick={newUpwell}>New Document</Button>
+              </NoDocument>
+            </div>
           )
         }}
       </Route>
