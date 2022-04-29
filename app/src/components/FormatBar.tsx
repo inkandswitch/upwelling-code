22b4c9d81b9abdc5d903df6cb58578d36bb6c807
chris <daiyitastic@gmail.com>
2022-04-29 20:50:13 +0200

    add header, paragraph, and text icons

--- app/src/components/FormatBar.tsx
+++ app/src/components/FormatBar.tsx
@@ -6,6 +6,10 @@ import { IconButton } from '@mui/material'
 import { editorSharedCSS } from './Editor'
 import { ReactComponent as Bold } from '../components/icons/format/Bold.svg'
 import { ReactComponent as Italic } from '../components/icons/format/Italic.svg'
+import { ReactComponent as H1 } from '../components/icons/format/H1.svg'
+import { ReactComponent as H2 } from '../components/icons/format/H2.svg'
+import { ReactComponent as H3 } from '../components/icons/format/H3.svg'
+import { ReactComponent as Text } from '../components/icons/format/Text.svg'
 
 export default function FormatBar() {
   return (
@@ -27,6 +31,18 @@ export default function FormatBar() {
         </IconButton>
         <IconButton aria-label="italic" title="ctrl-i or cmd-i">
           <Italic />
+        </IconButton>
+        <IconButton aria-label="heading 1" title="ctrl-alt-1">
+          <H1 />
+        </IconButton>
+        <IconButton aria-label="heading 2" title="ctrl-alt-2">
+          <H2 />
+        </IconButton>
+        <IconButton aria-label="heading 3" title="ctrl-alt-3">
+          <H3 />
+        </IconButton>
+        <IconButton aria-label="text" title="ctrl-alt-0">
+          <Text />
         </IconButton>
       </div>
     </div>
